709180a491ac00033ab44a7189eb112109390805
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-09 20:01:02 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match()
+  if phone.scan(/[\d]/).length == 10
     return true
+  else
+    return false
   end
 end
